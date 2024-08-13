0d2e3e9f87958d8e49025a7146f7cd981cb1083e
Kaush876 <kaushdev876@gmail.com>
2024-08-13 09:59:28 +0530

    first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      Welcome to EventEase, the ultimate destination for all your event planning needs! 
+      Whether you're orchestrating the wedding of your dreams, organizing a lively 
+      birthday celebration, hosting an exciting game night, or planning a relaxing camping getaway
+      , our platform offers everything you need to make your event a success. With a wide 
+      range of venues and services at your fingertips, EventEase simplifies the booking process,
+      allowing you to focus on what truly matters—enjoying the moment. Our dedicated 
+      team is here to ensure that every detail is tailored to your vision, creating unforgettable
+      experiences that you and your guests will cherish for a lifetime. Let 
+      EventEase take the stress out of planning, so you can celebrate with confidence and ease.
+      </p>
+    </section>
+  );
+};
+
+export default About;
